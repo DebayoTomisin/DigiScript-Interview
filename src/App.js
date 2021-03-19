3bd6236abef8fffcef28a56cb990590143ae2f72
Debayo Tomisin <debayotomisin07@gmail.com>
2021-03-18 21:05:58 -0700

    starter files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react"
-import './App.css'
+import React from 'react';
+import TodoApp from './components/TodoApp';
+import useStyles from './styles/AppStyles';
 
-function App(){
-  return(
-    <h1>This is the App component</h1>
-  )
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.App}>
+      <header className={classes.header}>
+        <h1>
+          Todo<span>list</span>
+        </h1>
+        <h2>A simple todolist app built with React and Context</h2>
+      </header>
+      <TodoApp />
+    </div>
+  );
 }
 
-export default App
+export default App;
